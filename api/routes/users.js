00028316ff0fa508b0810f46963cea3209a60210
Michael Giebner <mikegiebner@gmail.com>
2019-12-12 20:40:08 -0500

    Login now works with snackbar feedback.

--- api/routes/users.js
+++ api/routes/users.js
@@ -36,19 +36,30 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema)
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+/* Get A User */
+router.get('/', function (req, res) {
+  console.log("Get req.query: ", req.query)
+  User.findOne(req.query, function (err, foundUser) {
+    if (err) {
+      console.error("User Get Route Error: ", err)
+      res.send(err)
+    }
+    else {
+      console.log("User Get Route foundUser", foundUser)
+      res.send(foundUser)
+    }
+  })
 });
+
 /* Add A New User */
-router.post("/", function(req, res){
+router.post("/", function (req, res) {
   console.log("User Post Route req.body", req.body)
   User.create(req.body, (err, newUser) => {
-    if(err){
+    if (err) {
       console.error("User Post Route Error: ", err)
       res.send(err)
     }
-    else{
+    else {
       console.log("User Post Route newUser", newUser)
       res.send(newUser)
     }
