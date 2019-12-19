b2a09a79395dd5c30aea0cec6c70cb2abafd2dc0
Michael Giebner <mikegiebner@gmail.com>
2019-12-18 19:11:18 -0500

    Passed down name in props to each sidebar component so sidebar is modular instead of needing four different copies.

--- client/src/beer/components/Beer.js
+++ client/src/beer/components/Beer.js
@@ -3,16 +3,20 @@
 import React, { Component } from "react"
 import Sidebar from "../../components/Sidebar"
 import Mininavbar from "../../components/Mininavbar"
+import Cancel from "../../components/Cancel"
 import "../css/Beer.css"
 
 
-class Beer extends Component{
+class Beer extends Component {
 
-   render(){
-      return(
+   render() {
+      return (
          <div className="Beer-main-container">
-            <Sidebar />
-            <Mininavbar name={this.props.name}/>
+            <Sidebar name={this.props.name}/>
+            <Mininavbar name={this.props.name} />
+            <div className="Beer-cancel">
+               <Cancel />
+            </div>
          </div>
       )
    }
