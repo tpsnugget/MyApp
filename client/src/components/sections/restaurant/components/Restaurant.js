031b9ed1bf1fd167a1823995946c78b76369608d
Michael Giebner <mikegiebner@gmail.com>
2019-12-22 12:12:51 -0500

    Show pages now up for Restaurants and RVs.

--- client/src/components/sections/restaurant/components/Restaurant.js
+++ client/src/components/sections/restaurant/components/Restaurant.js
@@ -1,46 +1,73 @@
 /* Restaurant LANDING PAGE */
 
 import React, { Component } from "react"
+import axios from "axios"
 import PropTypes from "prop-types"
 import Sidebar from "../../../../components/Sidebar"
 import Mininavbar from "../../../../components/Mininavbar"
 import CancelLink from "../../../Atoms/CancelLink/CancelLink"
+import RestaurantShow from "./RestaurantShow"
 import "../css/Restaurant.css"
 
-class Restaurant extends Component{
+class Restaurant extends Component {
 
    static propTypes = {
       /* name comes from App.js, sent to Sidebar and Mininavbar */
       name: PropTypes.string,
 
-            /* Passed down from App.js. Used to determine if active user is the one
-         who added the selected RESTAURANT to the db. They are the only one who can
-         Edit or Delete the selected RESTAURANT. */
-         loggedInName: PropTypes.string
+      /* Passed down from App.js. Used to determine if active user is the one
+   who added the selected RESTAURANT to the db. They are the only one who can
+   Edit or Delete the selected RESTAURANT. */
+      loggedInName: PropTypes.string
    }
 
-   constructor(props){
+   constructor(props) {
       super(props)
       this.state = {
          chosenId: "",
          data: []
       }
+      this.handleClick = this.handleClick.bind(this)
    }
 
+   handleClick(id) {
+      this.setState({
+         chosenId: id
+      })
 
-   render(){
+      axios.get("http://localhost:9000/restaurant", {
+         params: {
+            _id: id
+         }
+      })
+         .then((response) => {
+            if (response.data === "") {
+               console.log("axios.get not in the db")
+            } else {
+               this.setState({
+                  data: response.data[0]
+               })
+            }
+         })
+         .catch((err) => console.log(err))
+   }
 
-      const { name } = this.props
-      const { chosenId } = this.state
+   render() {
 
-      return(
+      const { chosenId, data } = this.state
+      const { addedBy } = this.state.data
+      const { loggedInName, name } = this.props
+
+      const allowedToModifySelection = (addedBy === loggedInName ? true : false)
+
+      return (
          <div className="Restaurant-main-container">
             <div className="Restaurant-nav-container">
                <Sidebar name={name} select={this.handleClick} />
                <div className="Restaurant-inner-container">
-                  <Mininavbar name={name} chosenId={chosenId} />
+                  <Mininavbar name={name} chosenId={chosenId} allowedToModifySelection={allowedToModifySelection} />
                   <div>
-                     {/* {(chosenId !== "") && <RecipeShow data={data} />} */}
+                     {(chosenId !== "") && <RestaurantShow data={data} />}
                   </div>
                   <div className="Restaurant-cancel">
                      <CancelLink />
